b46dad23d70dcec3fb8ba2b986e3894022392e11
tyage <namatyage@gmail.com>
2014-10-06 12:49:47 +0900

    added text

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@ $(function() {
     el: '#graph',
     data: {
       fields: [
-        { title: 'Therapeutic Machine', x: 21, y: 21, r: 10 },
-        { title: 'Plant Factory', x: 10, y: 31, r: 5 },
+        { title: 'TM', x: 300, y: 150, r: 100 },
+        { title: 'PF', x: 380, y: 300, r: 75 },
+        { title: 'CM', x: 500, y: 380, r: 75 },
+        { title: 'FR', x: 80, y: 350, r: 80 },
+        { title: 'EQ', x: 360, y: 500, r: 80 },
+        { title: 'SD', x: 200, y: 650, r: 80 },
+        { title: 'OR', x: 880, y: 160, r: 80 },
+        { title: 'GD', x: 880, y: 360, r: 80 }
       ]
     }
   });
