0bb298706755eebe9db0de870fda88de973a761d
tyage <namatyage@gmail.com>
2014-10-07 07:16:41 +0900

    popup selected field

--- js/app.js
+++ js/app.js
@@ -3,15 +3,23 @@ $(function() {
     el: '#graph',
     data: {
       fields: [
-        { title: 'TM', x: 300, y: 100, r: 100 },
-        { title: 'PF', x: 380, y: 250, r: 75 },
-        { title: 'CM', x: 500, y: 330, r: 75 },
-        { title: 'FR', x: 80, y: 300, r: 80 },
-        { title: 'EQ', x: 360, y: 450, r: 80 },
-        { title: 'SD', x: 200, y: 600, r: 80 },
-        { title: 'OR', x: 880, y: 110, r: 80 },
-        { title: 'GD', x: 880, y: 310, r: 80 }
+        { title: 'TM', x: 300, y: 100, r: 100, people: ['test1', 'test2'] },
+        { title: 'PF', x: 380, y: 250, r: 75, people: ['test1', 'test2'] },
+        { title: 'CM', x: 500, y: 330, r: 75, people: ['test1', 'test2'] },
+        { title: 'FR', x: 80, y: 300, r: 80, people: ['test1', 'test2'] },
+        { title: 'EQ', x: 360, y: 450, r: 80, people: ['test1', 'test2'] },
+        { title: 'SD', x: 200, y: 600, r: 80, people: ['test1', 'test2'] },
+        { title: 'OR', x: 880, y: 110, r: 80, people: ['test1', 'test2'] },
+        { title: 'GD', x: 880, y: 310, r: 80, people: ['test1', 'test2'] }
       ]
+    },
+    methods: {
+      select: function(field) {
+        this.selectedField = field.$data;
+      },
+      unselect: function() {
+        this.selectedField = null;
+      }
     }
   });
 });
