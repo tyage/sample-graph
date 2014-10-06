1b78f4662139a3560cddf2810d54259a3013f1a9
tyage <namatyage@gmail.com>
2014-10-06 11:26:00 +0900

    watch less with gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+
+var CSS_ROOT = './css/';
+var LESS_ROOT = './less/';
+
+gulp.task('less', function() {
+  return gulp.src(LESS_ROOT + 'style.less')
+    .pipe(less({
+      compress: true
+    }))
+    .pipe(gulp.dest(CSS_ROOT));
+});
+gulp.task('watch', function() {
+  gulp.watch([LESS_ROOT + '*.less'], ['less']);
+});
+gulp.task('default', ['less', 'watch']);
