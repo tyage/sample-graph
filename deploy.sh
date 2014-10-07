10c6a004e2999e5a47709658ed961e8bbf615eca
tyage <namatyage@gmail.com>
2014-10-07 14:25:32 +0900

    added shell script for deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git checkout gh-pages
+git merge master
+npm install
+./node_modules/.bin/bower install
+./node_modules/.bin/gulp
+git add -A
+git commit -m "update site"
+git push origin gh-pages
