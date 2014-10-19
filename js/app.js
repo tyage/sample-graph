f2db32ba6e6015e7fb948cfe44a40bc2f332f96d
tyage <namatyage@gmail.com>
2014-10-19 19:38:04 +0900

    new design

--- js/app.js
+++ js/app.js
@@ -4,20 +4,28 @@ $(function() {
     data: {
       fields: [
         [
-          { title: 'TM', x: 350, y: 150, r: 100, people: ['test1', 'test2'] },
-          { title: 'PF', x: 430, y: 300, r: 75, people: ['test1', 'test2'] },
-          { title: 'CM', x: 550, y: 380, r: 75, people: ['test1', 'test2'] },
-          { title: 'FR', x: 130, y: 350, r: 80, people: ['test1', 'test2'] },
-          { title: 'EQ', x: 410, y: 500, r: 80, people: ['test1', 'test2'] },
-          { title: 'SD', x: 250, y: 650, r: 80, people: ['test1', 'test2'] },
-          { title: 'OR', x: 930, y: 160, r: 80, people: ['test1', 'test2'] },
-          { title: 'GD', x: 930, y: 360, r: 80, people: ['test1', 'test2'] }
+          { title: 'A', breakedTitles: ['A'],
+            x: 170, y: 79, r: 50, people: ['test1', 'test2'] },
+          { title: 'B', breakedTitles: ['B'],
+            x: 217, y: 150, r: 35, people: ['test1', 'test2'] },
+          { title: 'C', breakedTitles: ['C'],
+            x: 281, y: 181, r: 35, people: ['test1', 'test2'] },
+          { title: 'D', breakedTitles: ['D'],
+            x: 60, y: 170, r: 40, people: ['test1', 'test2'] },
+          { title: 'E', breakedTitles: ['E'],
+            x: 245, y: 253, r: 45, people: ['test1', 'test2'] },
+          { title: 'F', breakedTitles: ['F'],
+            x: 161, y: 233, r: 40, people: ['test1', 'test2'] }
         ], [
-          { title: 'GD', x: 880, y: 310, r: 80, people: ['test1', 'test2'] }
+          { title: 'tests', x: 470, y: 168, r: 38, people: ['test1', 'test2'] }
         ]
       ],
       school: 0,
-      language: 'ja'
+      language: 'ja',
+      axis: {
+        x: { start: 25, middle: 480, end: 935 },
+        y: { start: 25, middle: 248, end: 410 }
+      }
     },
     methods: {
       select: function(field) {
@@ -28,8 +36,8 @@ $(function() {
         this.selectedField = field.$data;
 
         // transition to selected field
-        var x = this.selectedField ? (850 - this.selectedField.x) : 0;
-        var y = this.selectedField ? (750 - this.selectedField.y) : 0;
+        var x = this.selectedField ? (480 - this.selectedField.x) : 0;
+        var y = this.selectedField ? (460 - this.selectedField.y) : 0;
         d3.select('#graph-group').transition()
           .attr('transform', 'translate(' + x + ', ' + y + ')');
       },
